68432a82cafaf8af8cf9d5b29f1a689a687c8acd
tree <tree081996@gmail.com>
2018-12-26 20:04:48 +0800

    快排

---
+++ Chapter6/QuickSort.py
@@ -0,0 +1,49 @@
+# -*- coding:utf-8 -*-
+
+"""快速排序
+"""
+
+
+def quick_sort(arr, left, right):
+    """快速排序
+
+    Args:
+        arr(list): 数组
+        left(int): 数组第一个元素下标
+        right(int): 数组最后一个元素下标
+    """
+    if left < right:
+        pivot = partition(arr, left, right)
+        quick_sort(arr, left, pivot-1)
+        quick_sort(arr, pivot+1, right)
+
+
+def partition(arr, left, right):
+    """划分
+
+    Args:
+        arr(list): 数组
+        left(int): 数组第一个元素下标
+        right(int): 数组最后一个元素下标
+
+    Returns:
+        pivot(int): 划分
+    """
+    pivot = arr[left]
+    i, j = left, right
+
+    while i < j:
+        while i < j and arr[i] < pivot:
+            i += 1
+        while i < j and arr[j] > pivot:
+            j -= 1
+        arr[i], arr[j] = arr[j], arr[i]
+    arr[i] = pivot
+
+    return i
+
+
+if __name__ == '__main__':
+    arr = [3, 2, 5, 1, 4, 7, 9, 0]
+    quick_sort(arr, 0, len(arr)-1)
+    print(arr)
